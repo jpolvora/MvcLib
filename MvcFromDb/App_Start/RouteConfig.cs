d326830647c47b9f8332cd5ce195b00a0e01fedf
jpolvora <jpolvora@gmail.com>
2014-08-24 08:48:17 -0400

    ok

--- MvcFromDb/App_Start/RouteConfig.cs
+++ MvcFromDb/App_Start/RouteConfig.cs
@@ -12,7 +12,7 @@ namespace MvcFromDb
         public static void RegisterRoutes(RouteCollection routes)
         {
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
-            routes.RouteExistingFiles = true;
+            //routes.RouteExistingFiles = true;
 
             routes.MapRoute(
                 name: "Default",
