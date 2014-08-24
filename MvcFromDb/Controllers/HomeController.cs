0df3b3c6f269082a9929ccd46102a3ef3d263a74
jpolvora <jpolvora@gmail.com>
2014-08-24 09:23:38 -0400

    clear cache before unload

--- MvcFromDb/Controllers/HomeController.cs
+++ MvcFromDb/Controllers/HomeController.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.Web.Caching;
 using System.Web.Mvc;
 
 namespace MvcFromDb.Controllers
@@ -29,6 +30,7 @@ namespace MvcFromDb.Controllers
 
         public ActionResult Reset()
         {
+            Infra.Misc.CacheWrapper.Clear();
             HttpRuntime.UnloadAppDomain();
             return new HttpStatusCodeResult(200);
         }
