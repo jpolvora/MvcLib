47c9d5b573778dd061e073ffb34a8303f0f092f9
jpolvora <jpolvora@gmail.com>
2014-08-25 03:39:04 -0400

    mvctracefilter

--- MvcFromDb/App_Start/FilterConfig.cs
+++ MvcFromDb/App_Start/FilterConfig.cs
@@ -1,5 +1,6 @@
 ﻿using System.Web;
 using System.Web.Mvc;
+using MvcFromDb.Infra;
 
 namespace MvcFromDb
 {
@@ -8,6 +9,7 @@ namespace MvcFromDb
         public static void RegisterGlobalFilters(GlobalFilterCollection filters)
         {
             filters.Add(new HandleErrorAttribute());
+            filters.Add(new MvcTracerFilter());
         }
     }
 }
---
+++ MvcFromDb/Infra/MvcTracerFilter.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Mvc.Filters;
+
+namespace MvcFromDb.Infra
+{
+    public class MvcTracerFilter : ActionFilterAttribute, IAuthorizationFilter, IExceptionFilter, IAuthenticationFilter
+    {
+        public void OnAuthorization(AuthorizationContext filterContext)
+        {
+            Trace.TraceInformation("[MvcTracerFilter]:[OnAuthorization]: {0}", filterContext.Controller);
+        }
+
+        public void OnException(ExceptionContext filterContext)
+        {
+            Trace.TraceError("[MvcTracerFilter]:[OnException]: {0}", filterContext.Exception);
+        }
+
+        public void OnAuthentication(AuthenticationContext filterContext)
+        {
+            Trace.TraceInformation("[MvcTracerFilter]:[OnAuthentication]: {0}", filterContext.Controller);
+        }
+
+        public void OnAuthenticationChallenge(AuthenticationChallengeContext filterContext)
+        {
+            Trace.TraceInformation("[MvcTracerFilter]:[OnAuthenticationChallenge]: {0}", filterContext.Controller);
+        }
+
+        public override void OnActionExecuting(ActionExecutingContext filterContext)
+        {
+            Trace.TraceInformation("[MvcTracerFilter]:[OnActionExecuting]: {0}/{1}", filterContext.Controller, filterContext.ActionDescriptor.ActionName);
+        }
+
+        public override void OnActionExecuted(ActionExecutedContext filterContext)
+        {
+            Trace.TraceInformation("[MvcTracerFilter]:[OnActionExecuted]: {0}/{1}", filterContext.Controller, filterContext.ActionDescriptor.ActionName);
+        }
+
+        public override void OnResultExecuting(ResultExecutingContext filterContext)
+        {
+            Trace.TraceInformation("[MvcTracerFilter]:[OnResultExecuting]: {0}/{1}", filterContext.Controller, filterContext.Result);
+        }
+
+        public override void OnResultExecuted(ResultExecutedContext filterContext)
+        {
+            Trace.TraceInformation("[MvcTracerFilter]:[OnResultExecuted]: {0}/{1}", filterContext.Controller, filterContext.Result);
+        }
+    }
+}
