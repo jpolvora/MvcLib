bd6b0caa5f00c8da44b315f33a24444126af8301
jpolvora <jpolvora@gmail.com>
2014-08-26 02:40:43 -0400

    change appharbor main solution

--- MvcFromDbMinimal/Global.asax.cs
+++ MvcFromDbMinimal/Global.asax.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Diagnostics;
 using System.Web.Mvc;
 using System.Web.Optimization;
 using System.Web.Routing;
@@ -14,6 +15,12 @@ namespace MvcFromDbMinimal
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
+
+            var traceOutput = Server.MapPath("~/traceOutput.log");
+            Trace.Listeners.Add(new TextWriterTraceListener(traceOutput));
+            Trace.AutoFlush = true;
+            Trace.Flush();
+
         }
 
         protected void Session_Start(object sender, EventArgs e)
