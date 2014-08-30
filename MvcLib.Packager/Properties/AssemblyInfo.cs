16ba06d69ab3c78364a87223a045ddcc31716558
jpolvora <jpolvora@gmail.com>
2014-08-29 22:19:26 -0400

    myget

--- MvcLib.Packager/Properties/AssemblyInfo.cs
+++ MvcLib.Packager/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("MvcLib.Nuget")]
+[assembly: AssemblyTitle("MvcLib.Packager")]
 [assembly: AssemblyDescription("Project template made by EyeCatch (http://www.eyecatch.no/)")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("MvcLib.Nuget")]
+[assembly: AssemblyProduct("MvcLib.Packager")]
 [assembly: AssemblyCopyright("Copyright ©  2014")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
